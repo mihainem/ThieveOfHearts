d191d25b4c0dd76d47000ddc88a9a3d39f6ee89c
mihainem <dan_y27@yahoo.com>
2021-02-27 11:54:48 +0200

    adding collectables, fixed levelTiles textures

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour, ICollectable
+{
+    public void FlyToRecipient()
+    {
+        Debug.Log("Flying To recipient");
+    }
+
+    public void UpdateCounter()
+    {
+        Debug.Log("Updating Counter");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FlyToRecipient();
+        UpdateCounter();
+    }
+}
---
+++ Assets/Scripts/ResourcesManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesManager : MonoBehaviour
+{
+    private static ResourcesManager instance;
+    public static ResourcesManager Instance { get { return instance;  } }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+}
