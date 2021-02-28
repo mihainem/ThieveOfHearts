64b3a1a5d3a680fa0032043079eaeb5edc10caac
mihainem <dan_y27@yahoo.com>
2021-02-28 23:28:37 +0200

    added win and lose panels

---
+++ Assets/Scripts/UI/MessagePanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MessagePanel : Window
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    public void SetText(string message) 
+    {
+        text.text = message;
+    }
+}
